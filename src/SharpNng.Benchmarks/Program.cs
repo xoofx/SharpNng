2d4bdaf0c6ed709fe856ec98ae6691f1cb595b55
Alexandre Mutel <alexandre_mutel@live.com>
2021-11-20 18:23:07 +0100

    Update benchmark

--- src/SharpNng.Benchmarks/Program.cs
+++ src/SharpNng.Benchmarks/Program.cs
@@ -1,99 +1,31 @@
 ﻿using System.Diagnostics;
 using static nng;
 
-string ipcName;
 const int count = 100000;
-
-void Server()
-{
-    nng_socket sock = default;
-
-    int result = nng_rep0_open(ref sock);
-    nng_assert(result);
-    try
-    {
-        Console.Out.WriteLine($"Server: Starting {ipcName}");
-
-        nng_listener listener = default;
-        result = nng_listen(sock, ipcName, ref listener, 0);
-        nng_assert(result);
-
-        Console.Out.WriteLine("Server: Listening");
-
-        for (int i = 0; i < count; i++)
-        {
-            IntPtr buf;
-            size_t sz = default;
-            unsafe
-            {
-                result = nng_recv(sock, new IntPtr(&buf), ref sz, NNG_FLAG_ALLOC);
-                nng_assert(result);
-            }
-            nng_free(buf, sz);
-        }
-    }
-    finally
-    {
-        nng_close(sock);
-        Console.WriteLine("Server: Closed");
-    }
-};
-
-void Client()
-{
-    Console.Out.WriteLine("Client: Started");
-
-    nng_socket sock = default;
-
-    int result = nng_req0_open(ref sock);
-    nng_assert(result);
-
-    try
-    {
-        nng_dialer dialer = default;
-        for (int i = 0; i < 10; i++)
-        {
-            result = nng_dial(sock, ipcName, ref dialer, 0);
-            if (result == 0) break;
-            Console.WriteLine("Client: dial failed, waiting for server to listen - sleep 100ms");
-            Thread.Sleep(100);
-        }
-
-        nng_assert(result);
-
-        Console.Out.WriteLine("Client: Connected");
-        Console.Out.WriteLine("Client: Sending");
-        for (int i = 0; i < count; i++)
-        {
-            unsafe
-            {
-                int value = 0x6afedead;
-                result = nng_send(sock, new IntPtr(&value), 4, 0);
-                nng_assert(result);
-            }
-        }
-    }
-    finally
-    {
-        nng_close(sock);
-        Console.WriteLine("Client: Closed");
-    }
-};
-
 
 if (args.Length == 2)
 {
     switch (args[0])
     {
         case "--server":
-            ipcName = args[1];
-            Server();
+            Server(args[1]);
             break;
     }
 }
 else
 {
-    ipcName = $"ipc:///tmp/SharpNngBenchmarks_{Guid.NewGuid():N}.ipc";
+    foreach (var size in new int[] { 128, 1024, 16384 })
+    {
+        Benchmark($"ipc:///tmp/SharpNngBenchmarks_{Guid.NewGuid():N}.ipc", size);
+        Benchmark($"tcp://127.0.0.1:6001", size);
+    }
+}
+
+static void Benchmark(string ipcName, int size)
+{
+    var benchKind = ipcName.Substring(0, ipcName.IndexOf(':'));
+    Console.WriteLine($"===========================================================================================");
+    Console.WriteLine($"Benchmarking {benchKind} - Data Size = {size}");
 
     var process = new Process();
     process.StartInfo = new ProcessStartInfo(Process.GetCurrentProcess().MainModule.FileName, $"--server {ipcName}")
@@ -117,12 +49,12 @@ else
     bool processTerminated = false;
     try
     {
-        Client();
+        Client(ipcName, 128);
         clock.Stop();
         process.WaitForExit(1000);
         processTerminated = true;
-        Console.WriteLine($"{((double)count) / clock.Elapsed.TotalSeconds} req/s");
-        Console.WriteLine($"{clock.Elapsed.TotalMilliseconds * 1000.0 / (double)count} μs/req");
+        Console.WriteLine($"{benchKind}: {((double)count) / clock.Elapsed.TotalSeconds} request_reply/s");
+        Console.WriteLine($"{benchKind}: {clock.Elapsed.TotalMilliseconds * 1000.0 / (double)count} μs/request_reply");
     }
     finally
     {
@@ -132,6 +64,85 @@ else
         }
     }
 }
+
+static void Server(string ipcName)
+{
+    nng_socket sock = default;
+
+    long sizeInBytesReceived = 0;
+
+    int result = nng_rep0_open(ref sock);
+    nng_assert(result);
+    try
+    {
+        Console.Out.WriteLine($"Server: Starting {ipcName}");
+
+        nng_listener listener = default;
+        result = nng_listen(sock, ipcName, ref listener, 0);
+        nng_assert(result);
+
+        Console.Out.WriteLine("Server: Listening");
+        for (int i = 0; i < count; i++)
+        {
+            // Receive the buffer
+            result = nng_recv(sock, out var buffer);
+            nng_assert(result);
+            sizeInBytesReceived += buffer.Length;
+            // Send the same buffer back
+            result = nng_send(sock, buffer.AsSpan());
+            nng_assert(result);
+            buffer.Dispose();
+        }
+    }
+    finally
+    {
+        nng_close(sock);
+        Console.WriteLine($"Server: Closed ({sizeInBytesReceived} bytes received)");
+    }
+};
+
+static void Client(string ipcName, int size)
+{
+    Console.Out.WriteLine("Client: Started");
+
+    nng_socket sock = default;
+
+    int result = nng_req0_open(ref sock);
+    nng_assert(result);
+    var buffer = new byte[size];
+
+    try
+    {
+        nng_dialer dialer = default;
+        for (int i = 0; i < 10; i++)
+        {
+            result = nng_dial(sock, ipcName, ref dialer, 0);
+            if (result == 0) break;
+            Console.WriteLine("Client: dial failed, waiting for server to listen - sleep 100ms");
+            Thread.Sleep(100);
+        }
+
+        nng_assert(result);
+
+        Console.Out.WriteLine("Client: Connected");
+        Console.Out.WriteLine("Client: Sending");
+        for (int i = 0; i < count; i++)
+        {
+            result = nng_send(sock, buffer);
+            nng_assert(result);
+
+            result = nng_recv(sock, out var recvbuffer);
+            nng_assert(result);
+            if (recvbuffer.Length != buffer.Length) throw new InvalidOperationException("Size is not matching");
+            recvbuffer.Dispose();
+        }
+    }
+    finally
+    {
+        nng_close(sock);
+        Console.WriteLine("Client: Closed");
+    }
+};
 
 
 static void server_ErrorDataReceived(object sender, DataReceivedEventArgs e)
